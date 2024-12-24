84cf0541c25bb497eae3ce1a8ffb2bbbc44af521
SimeDev <simedev92@gmail.com>
2024-12-24 20:02:57 +0100

    Primer commit - Proyecto Alerta Trabajo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'sobre-nosotros',  // Redirecciona solo la raíz (localhost:4200)
+    pathMatch: 'full',
+  },
+  {
+    path: 'sobre-nosotros',
+    loadComponent: () => import('./pages/about/about-page.component'),
+  },
+  {
+    path: 'contacto',
+    loadComponent: () => import('./pages/contact/contact-page-component'),
+  },
+  {
+    path: 'noticias',
+    loadComponent: () => import('./pages/pricing/pricing-page-component'),
+  },
+  {
+    path: '**',  // Rutas no definidas van aquí
+    redirectTo: '404',  // O redirige a una página 404 (si existe)
+  },
+];
