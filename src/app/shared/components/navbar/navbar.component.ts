8961af84cfc43e7a29c032511416ff5db5205391
SimeDev <simedev92@gmail.com>
2024-12-25 10:40:31 +0100

    Cambios en navbar experiencia de usuario

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -10,10 +10,27 @@ import { RouterLink } from '@angular/router';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
-
   isMenuOpen: boolean = false;
 
-  toggleMenu() {
+  constructor(private elementRef: ElementRef) {}
+
+  onMenuClick(event: Event): void {
+    event.stopPropagation();
+  }
+  toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
   }
- }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+
+  // Detectar clics fuera del menú
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (!clickedInside) {
+      this.closeMenu();
+    }
+  }
+}
