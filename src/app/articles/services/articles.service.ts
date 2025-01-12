25632a9bfe8bbc092f1a94a854a686047fc4ab1b
SimeDev <simedev92@gmail.com>
2025-01-12 14:53:19 +0100

    arreglo url prod

--- src/app/articles/services/articles.service.ts
+++ src/app/articles/services/articles.service.ts
@@ -1,49 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { forkJoin, map, Observable, switchMap, tap } from 'rxjs';
-import { Article, PaginatedArticles, SimpleArticle } from '../interfaces';
-
+import { forkJoin, map, Observable, switchMap } from 'rxjs';
+import { Article, PaginatedArticles } from '../interfaces';
+import { environments } from '../../../environments/environments';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArticlesService {
   private http = inject(HttpClient);
-  private baseUrl = 'http://localhost:3000/articles';
+  private baseUrl = environments.baseUrl;
 
   // Obtener artículos paginados (con descripciones)
   public loadPage(page: number = 1, limit: number = 6): Observable<PaginatedArticles> {
-    return this.http.get<PaginatedArticles>(`${this.baseUrl}?page=${page}&limit=${limit}`)
+    return this.http
+      .get<PaginatedArticles>(`${this.baseUrl}/articles/?page=${page}&limit=${limit}`)
       .pipe(
         map((response) => ({
           ...response,
-          articles: response.articles.map(article => ({
+          articles: response.articles.map((article) => ({
             ...article,
-            description: this.extractDescription(article)
-          }))
+            description: this.extractDescription(article),
+          })),
         }))
       );
   }
 
-
   // Obtener artículo por slug
   public getArticleBySlug(slugOrId: string): Observable<Article> {
-    return this.http.get<Article>(`${this.baseUrl}/${slugOrId}`).pipe(
-      switchMap(article =>
+    return this.http.get<Article>(`${this.baseUrl}/articles/${slugOrId}`).pipe(
+      switchMap((article) =>
         this.incrementViews(article._id).pipe(
           map(() => article)
         )
       )
     );
   }
+
   // Obtener artículo con detalles completos
   public loadArticleWithDetails(slug: string): Observable<{ article: Article; description: string }> {
     return forkJoin({
-      article: this.http.get<Article>(`${this.baseUrl}/${slug}`),
+      article: this.http.get<Article>(`${this.baseUrl}/articles/${slug}`),
     }).pipe(
       map(({ article }) => ({
         article,
-        description: this.extractDescription(article)
+        description: this.extractDescription(article),
       }))
     );
   }
@@ -59,7 +60,8 @@ export class ArticlesService {
     return 'Descripción no disponible';
   }
 
+  // Incrementar vistas de un artículo
   private incrementViews(id: string): Observable<void> {
-    return this.http.patch<void>(`${this.baseUrl}/${id}/increment-views`, {});
+    return this.http.patch<void>(`${this.baseUrl}/articles/${id}/increment-views`, {});
   }
 }
---
+++ src/environments/environments.ts
@@ -0,0 +1,7 @@
+export const environments = {
+  baseUrl: 'https://quixotic-nanine-aletatrabajo-29b23891.koyeb.app',
+
+}
+
+
+
